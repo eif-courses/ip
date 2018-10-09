f4322907f933badd1a3a4842b45ab57b0d684543
Marius Gegoevskis <m.gzegozevskis@eif.viko.lt>
2018-10-09 22:13:44 +0300

    ip adreso scriptas

---
+++ ip.ps1
@@ -0,0 +1,13 @@
+$IPV4 = Test-Connection -ComputerName (hostname) -Count 1  | Select -ExpandProperty IPV4Address
+
+
+("# "+$IPV4.IPAddressToString) > D:\get_my_ip\README.md
+
+ipconfig > D:\get_my_ip\ip.txt
+cd D:\get_my_ip\
+cd .git
+del index.lock
+cd ..
+git add . 
+git commit -m "ip adreso scriptas"
+git push -u origin master
