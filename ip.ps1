2b4af4190ffce144842c4c7d6d28cb98a1e05454
Marius Gegoevskis <m.gzegozevskis@eif.viko.lt>
2018-10-10 11:12:39 +0300

    EIF:IP-ADRESAS

--- ip.ps1
+++ ip.ps1
@@ -1,13 +1,10 @@
-$IPV4 = Test-Connection -ComputerName (hostname) -Count 1  | Select -ExpandProperty IPV4Address
-
-
-$IPV4.IPAddressToString > D:\get_my_ip\README.md
-
+$IPV4 = (netsh i ip sh ad)-match'Address'-replace'.+:\s+(\S+)','$1'
+$IPV4 > D:\get_my_ip\README.md
 ipconfig > D:\get_my_ip\ip.txt
 cd D:\get_my_ip\
 cd .git
 del index.lock
 cd ..
 git add . 
-git commit -m "ip adreso scriptas"
+git commit -m "EIF:IP-ADRESAS"
 git push -u origin master
